b6faca1f3febe0c83f120596e24afaa55f5925ec
Ed Blake <kitsu.eb@gmail.com>
2016-05-23 15:15:26 -0700

    Fix misbehavior with lost mouse up events.

--- src/pyqt4_testbed/core.cljs
+++ src/pyqt4_testbed/core.cljs
@@ -1,17 +1,18 @@
 (ns pyqt4-testbed.core
   (:require [cljs.core.async :as async
              :refer [<! >! timeout chan take! put!
-                     sliding-buffer alts!]]
+                     sliding-buffer close!]]
             [dommy.core :as dommy :refer-macros [sel sel1]])
   (:require-macros [cljs.core.async.macros :as async-macros
                     :refer [go go-loop do-alt alt!]]))
 
 (defn evt-chan [target kind btn]
+  ;; The sliding buffer probably isn't needed anymore?
   (let [out (chan (sliding-buffer 1))]
     (dommy/listen! target kind
                   (fn [evt]
                     (when (= (.-button evt) btn)
-                      (put! out evt))))
+                      (put! out [kind evt]))))
     out))
 
 (defn get-row-num [evt]
@@ -46,23 +47,47 @@
                           (dommy/toggle-class! row "marked")))))
                   initial))))
 
-(defn main []
-  ;; Setup some event input channels and start a process to watch them.
-  (let [target (sel1 :#doclist)
-        lmb-down-chan (evt-chan target :mousedown 0)
-        lmb-move-chan (evt-chan target :mousemove 0)
-        lmb-up-chan (evt-chan target :mouseup 0)]
-    ;; Wait for initial LMB down
-    (go-loop [evt (<! lmb-down-chan)]
-      (let [row (get-row-num evt)
-            highlight (get-highlight target)]
-        ;; Consume move events until we get an up event
-        (loop [[evt ch] (alts! [lmb-up-chan lmb-move-chan] {:priority true})]
-          ;; update highlighting...
-          (toggle-highlight highlight row (get-row-num evt))
-          (when-not (= ch lmb-up-chan)
-            (recur (alts! [lmb-up-chan lmb-move-chan] {:priority true})))))
+(defn build-drag-chan [target]
+  ;; Setup event source channels and start producing drag sub-channels
+  (let [output-chan (chan)
+        input-chan (async/merge [(evt-chan target :mousedown 0)
+                                 (evt-chan target :mousemove 0)
+                                 (evt-chan target :mouseup 0)
+                                 (evt-chan target :mouseleave 0)])
+        terminals #{:mouseup :mouseleave}]
+    (go-loop [[tag evt] (<! input-chan)]
+      (when (= tag :mousedown)
+        (let [drag-chan (chan)]
+          ;; Give new drag channel to consumers
+          (>! output-chan drag-chan)
+          (>! drag-chan evt)
+          ;; Consume move events until we get an up event
+          (loop [[tag evt] (<! input-chan)]
+            ;; Output events regardless of type
+            (>! drag-chan evt)
+            ;; Close drag on terminal event else recur
+            (if (contains? terminals tag)
+              (close! drag-chan)
+              (recur (<! input-chan))))))
       ;; Wait for next LMB down
-      (recur (<! lmb-down-chan)))))
+      (recur (<! input-chan)))
+    output-chan))
+
+(defn main []
+  ;; Get handle of target element and create drag channel
+  (let [target (sel1 :#doclist)
+        drag-chan (build-drag-chan target)]
+    ;; Loop forever pulling channels of drag events from drag-chan
+    (go-loop [drag-set (<! drag-chan)]
+      ;; Store the initial row and all highlight states
+      (let [row (get-row-num (<! drag-set))
+            highlight (get-highlight target)]
+        ;; Consume events from drag-set until it is closed
+        (loop [evt (<! drag-set)]
+         (when evt
+           ;; update highlighting...
+           (toggle-highlight highlight row (get-row-num evt))
+           (recur (<! drag-set)))))
+      (recur (<! drag-chan)))))
 
 (dommy/listen! js/window :load main)
